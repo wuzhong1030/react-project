e726ceb39f5f6f142ffe6e17b89cc8c64db6ee88
吴忠 <wuzhong1598@qq.com>
2018-11-10 15:41:07 +0800

    commit code

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
-import { HeaderWrapper } from './styled'
+import { HeaderWrapper, Logo, Nav, NavItem, NavSearch } from './styled'
 
 export default class MHeader extends Component {
     render() {
         return (
-            <HeaderWrapper>heee</HeaderWrapper>
+            <HeaderWrapper>
+                <Logo />
+                <Nav>
+                    <NavItem className='left'>发现</NavItem>
+                    <NavItem className='left'>关注</NavItem>
+                    <NavItem className='right'>消息</NavItem>
+                    <NavItem className='right'>搜索</NavItem>
+                    <NavSearch></NavSearch>
+                </Nav>
+            </HeaderWrapper>
         )
     }
 }
--- src/common/header/styled.js
+++ src/common/header/styled.js
@@ -1,8 +1,62 @@
 import styled from 'styled-components'
+import logoPic from '../../static/logo.png'
 
 export const HeaderWrapper = styled.div`
+    position: relative;
+    padding: 0 200px;
     height: 58px;
-    width: 768px;
-    margin: 0 auto;
+    border-bottom: 1px solid #f0f0f0;
     background-color: #fff;
+`
+
+
+export const Logo = styled.a.attrs({
+    href: '/'
+})`
+    position: absolute;
+    left: 0;
+    top: 0;
+    display: inline-block;
+    height: 58px;
+    width: 100px;
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    background-image: url(${logoPic});
+`
+
+export const Nav = styled.nav`
+    height: 100%;
+`
+
+export const NavItem = styled.div`
+    color: #333;
+    line-height: 56px;
+    font-size: 17px;
+    padding: 0 14px;
+    &.left {
+        float: left;
+        color: #969696;
+    }
+    &.right {
+        float: right;
+        color: #ea6f5a;
+    }
+`
+
+export const NavSearch = styled.input.attrs({
+    placeholder: '搜索'
+})`
+    width: 160px;
+    height: 38px;
+    margin-top: 9px;
+    margin-left: 20px;
+    outline: none;
+    border: none;
+    border-radius: 19px;
+    padding: 0 20px;
+    background: #eee;
+    font-size: 14px;
+    &::placeholder {
+        color: #999;
+    }
 `
