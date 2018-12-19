efab7ab364196bdb1f87e046cfb836d985cdbc19
wuzhong1598@qq.com <wuzhong1598@qq.com>
2018-12-19 14:57:45 +0800

    add code

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,18 +1,21 @@
 const defaultState = {
-	focused: false
-}
+  focused: false
+};
 
 export default (state = defaultState, action) => {
-	let { type } = action
-	switch (type) {
-		case 'search_focus':
-			return {
-				focused: true
-			}
-		case 'search_blur':
-			return {
-				focused: false
-			}
-	}
-	return state
-}
+  let { type } = action;
+  switch (type) {
+    case "search_focus":
+      return {
+        focused: true
+      };
+    case "search_blur":
+      return {
+        focused: false
+	  };
+	default:
+	  return {
+		  focused: false
+	  }
+  }
+};
